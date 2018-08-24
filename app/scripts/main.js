3d16ab3f7f109d8d69a5426abf9edabbdf800307
Christine Hager <cehager@gmail.com>
2018-08-24 16:50:02 -0700

    Updates formatting of recipe card, and navbar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,28 @@
 
 // Helper functions for recipes data
 
+$(document).ready(() => {
+
+  // Format contentful json data
+  let ingredients = $('.ingredients');
+  let prep = $('.preparation');
+
+  for (let i = 0; i < ingredients.length; i++) {
+    ingredients[i].innerHTML = $(window)[0].marked(ingredients[i].textContent);
+    prep[i].innerHTML = $(window)[0].marked(prep[i].textContent);
+  }
+
+  // Transition to smaller header on scroll
+  $(document).scroll(() => {
+    if ($(document).scrollTop() > 90) {
+      $('.header').addClass('shrink');
+    } else {
+      $('.header').removeClass('shrink');
+    }
+  });
+
+})
+
 const recipesArray = (allRecipes) => {
   
 };
