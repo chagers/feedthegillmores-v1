6b3236494f1c7e1cbee48f542ff29074ebb8657d
Christine Hager <cehager@gmail.com>
2018-08-28 17:03:35 -0700

    Updates filename conventions, continues recipe detail render

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,6 @@
 'use strict';
 const pug = require('pug');
 
-// Helper functions for recipes data
-
 $(document).ready(() => {
 
   // Format contentful json data
@@ -23,19 +21,30 @@ $(document).ready(() => {
     }
   });
 
+  // TODO: move this to another script file, and just load on the recipe page.
+  // Can get the recipe id from the url params, then pass as locals in compile
+  
   // Render recipe detail page
   let recipeId;
-  $('.card').click(function() {
-    recipeId = $(this)[0].id;
+  // $('.card').click(function() {
+  //   recipeId = $(window)[0].location.hash;
+  //   console.log(recipeId);
+  // });
 
-    const compileDetail = pug.compile('this is a recipe');
+  const compileDetail = pug.compile('this is a recipe');
 
-    $('#recipeDetail').innerHTML = compileDetail({ locals: { id: recipeId }});
+  const recipeDetail = () => {
+    recipeId = $(window)[0].location.search.substr(1);
+    console.log(recipeId);
+    if (window.location.search.substr(1) === recipeId) {
+      console.log('getting here');
+      $('#recipeDetail').innerHTML[0] = compileDetail({ locals: { id: recipeId }});
+    }
+  }
 
-    // window.location.href = "/recipe-detail.html";
-  });
+  window.addEventListener('hashchange', recipeDetail(), false);
 
-})
+});
 
 const recipesArray = (allRecipes) => {
   
